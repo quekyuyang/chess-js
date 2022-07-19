ec3e57d8e01d53e549cb8a28f457aee4ff7aa79c
Yu Yang Quek <quekyuyang@gmail.com>
2022-07-19 07:52:20 +0200

    Move doubly nested functions out

--- UI.mjs
+++ UI.mjs
@@ -6,7 +6,6 @@ function addPickupEvent(elem, renderer, move_manager, next_turn_callback) {
 
   function pickup(event) {
     if (move_manager.is_movable(this.id)) {
-      let piece = this;
       let bound_rect = this.getBoundingClientRect();
 
       this.style.width = bound_rect.width;
@@ -20,27 +19,27 @@ function addPickupEvent(elem, renderer, move_manager, next_turn_callback) {
       let chessboard = document.querySelector(".chessboard");
       chessboard.addEventListener("mousemove", move);
       this.addEventListener("mouseup", drop);
+    }
+  }
 
-      function move(event) {
-        piece.style.left = event.pageX;
-        piece.style.top = event.pageY;
-      }
+  function move(event) {
+    elem.style.left = event.pageX;
+    elem.style.top = event.pageY;
+  }
 
-      function drop(event) {
-        chessboard.removeEventListener("mousemove", move);
-        this.removeEventListener("mouseup", drop);
-        renderer.clear_moves();
-        this.style.position = "relative";
-        this.style.left = "50%";
-        this.style.top = "50%";
-        let elements_at_pos = document.elementsFromPoint(event.pageX, event.pageY);
-        for (let element of elements_at_pos) {
-          if (element.classList.contains("square")) {
-            if (move_manager.move_piece(this.id, new Vector(parseInt(element.dataset.col), parseInt(element.dataset.row))))
-              next_turn_callback();
-            break;
-          }
-        }
+  function drop(event) {
+    document.querySelector(".chessboard").removeEventListener("mousemove", move);
+    this.removeEventListener("mouseup", drop);
+    renderer.clear_moves();
+    this.style.position = "relative";
+    this.style.left = "50%";
+    this.style.top = "50%";
+    let elements_at_pos = document.elementsFromPoint(event.pageX, event.pageY);
+    for (let element of elements_at_pos) {
+      if (element.classList.contains("square")) {
+        if (move_manager.move_piece(this.id, new Vector(parseInt(element.dataset.col), parseInt(element.dataset.row))))
+          next_turn_callback();
+        break;
       }
     }
   }
